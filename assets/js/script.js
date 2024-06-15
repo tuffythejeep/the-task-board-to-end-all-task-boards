b5395ed51a50778c7d5f52c8e43b082425fccfe9
tuffythejeep <tuffythejeep@yahoo.com>
2024-06-14 22:03:32 -0500

    updating my script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,37 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-    const timestamp = Date.now(); // this will get me the current timestamp
-    const randomNum = Math.floor(Math.random() * 1000); // Generate a random number less than 1000
-    const uniqueId = `${timestamp}-${randomNum}`; // this will combine timestamp and random number
+  const timestamp = Date.now(); // this will get me the current timestamp
+  const randomNum = Math.floor(Math.random() * 1000); // Generate a random number less than 1000
+  const uniqueId = `${timestamp}-${randomNum}`; // this will combine timestamp and random number
 
-    return uniqueId;
+  return uniqueId;
 }
 
-const taskId = generateTaskId ();
+const taskId = generateTaskId();
 console.log(taskId);
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {}
+function createTaskCard(taskId, taskTitle, taskDescription, taskDueDate) {
+  const taskCard = {
+    id: taskId,
+    title: taskTitle,
+    description: taskDescription,
+    dueDate: taskDueDate,
+  };
+
+  // The below will return the task card object
+  return taskCard;
+}
+
+const newTaskCard = createTaskCard(
+  "uniqueTaskId",
+  "Task Title",
+  "Task Description",
+  "Due Date"
+);
+console.log(newTaskCard);
+// Output: should be { id: 'uniqueTaskId', title: 'Task Title', description: 'Task Description', dueDate: 'Due Date'}
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {}
